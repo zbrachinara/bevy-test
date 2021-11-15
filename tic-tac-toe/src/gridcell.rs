c235500023074865a329022f89a17b24634d97d4
zbrachinara <zbrachinara@gmail.com>
2021-11-15 18:29:01 -0500

    made initial version of GridCellBundle

--- tic-tac-toe/src/gridcell.rs
+++ tic-tac-toe/src/gridcell.rs
@@ -1,8 +1,71 @@
+use bevy::ecs::system::Command;
 use bevy::prelude::*;
-use bevy_svg::prelude::SvgBundle;
+use bevy_svg::prelude::*;
+use std::path::Path;
 
-pub struct AddGridCell {
-    transform: Transform,
+pub struct AddGridCell<P>
+where P: AsRef<Path> + Send + Sync + 'static
+{
+    pub pos: Pos,
+    pub red: P,
+    pub blue: P,
+    pub bbox: P,
+}
+
+impl<P: AsRef<Path> + Send + Sync + 'static> Command for AddGridCell<P> {
+    fn write(self: Box<Self>, world: &mut World) {
+        let transform = Vec3::new(
+            self.pos.0 as f32 * 200f32,
+            self.pos.1 as f32 as f32 * 200f32,
+            0.0,
+        );
+
+        let red = world
+            .spawn()
+            .insert_bundle(
+                SvgBuilder::from_file(self.red)
+                    .position(transform)
+                    .build()
+                    .unwrap(),
+            )
+            .id();
+        let blue = world
+            .spawn()
+            .insert_bundle(
+                SvgBuilder::from_file(self.blue)
+                    .position(transform)
+                    .build()
+                    .unwrap(),
+            )
+            .id();
+        let bbox = world
+            .spawn()
+            .insert_bundle(
+                SvgBuilder::from_file(self.bbox)
+                    .position(transform)
+                    .build()
+                    .unwrap(),
+            )
+            .id();
+
+        // world
+        //     .entity_mut(red.clone())
+        //     .get_mut::<Visible>()
+        //     .map(|mut visible| {
+        //         visible.is_transparent = true;
+        //     });
+
+        let cell = world
+            .spawn()
+            .insert_bundle(GridCellBundle {
+                pos: self.pos,
+                transform: Transform::from_translation(transform),
+                ..Default::default()
+            })
+            .id();
+
+        world.entity_mut(cell).push_children(&[red, blue, bbox]);
+    }
 }
 
 #[derive(Bundle, Default)]
@@ -22,6 +85,7 @@ enum Player {
 #[derive(Default)]
 struct GridCell;
 
-struct Pos(u8, u8);
+#[derive(Default)]
+pub struct Pos(pub u8, pub u8);
 
 struct ClickedBy(Player);
--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 use bevy_svg::prelude::*;
+use crate::gridcell::Pos;
 
 mod gridcell;
 
@@ -8,12 +9,15 @@ fn make_scene(
     mut commands: Commands,
 ) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.add(gridcell::AddGridCell{
+        pos: Pos(0, 0),
+        red: "tic-tac-toe/assets/red_mark.svg",
+        blue: "tic-tac-toe/assets/blue_mark.svg",
+        bbox: "tic-tac-toe/assets/empty_cell.svg",
+    })
 
-    let blue = SvgBuilder::from_file("tic-tac-toe/assets/blue_mark.svg")
-        .position(Default::default())
-        .build()
-        .unwrap();
-    commands.spawn_bundle(blue);
+    // commands.spawn_bundle(blue);
+
 }
 
 fn main() {
