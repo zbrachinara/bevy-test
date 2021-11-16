bc0b1363d277e1a326e62b92e15b920a5144f343
zbrachinara <zbrachinara@gmail.com>
2021-11-15 21:00:27 -0500

    attempt to spawn multiple instances of cell

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,23 +1,41 @@
 use bevy::prelude::*;
+use std::path::{Path, PathBuf};
 
-use bevy_svg::prelude::*;
 use crate::gridcell::Pos;
+use bevy_svg::prelude::*;
 
 mod gridcell;
 
-fn make_scene(
-    mut commands: Commands,
-) {
+fn make_scene(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.add(gridcell::AddGridCell{
-        pos: Pos(0, 0),
-        red: "tic-tac-toe/assets/red_mark.svg",
-        blue: "tic-tac-toe/assets/blue_mark.svg",
-        bbox: "tic-tac-toe/assets/empty_cell.svg",
-    })
+    let root = PathBuf::from("tic-tac-toe/assets");
+    let (red, blue, bbox) = {
+        let (mut red, mut blue, mut bbox) = (
+            root.clone(),
+            root.clone(),
+            root.clone(),
+        );
+
+        red.push("red_mark.svg");
+        blue.push("blue_mark.svg");
+        bbox.push("empty_cell.svg");
+
+        (red, blue, bbox)
+    };
+
+    (0..3).for_each(|x| {
+        (0..3).for_each(|y| {
+            commands.add(gridcell::AddGridCell {
+                pos: Pos(x, y),
+                red: red.clone(),
+                blue: blue.clone(),
+                bbox: bbox.clone(),
+            })
+        });
+    });
+
 
     // commands.spawn_bundle(blue);
-
 }
 
 fn main() {
