166c5bc06578d85d8aa1a7698cb8329edceae59d
zbrachinara <zbrachinara@gmail.com>
2021-11-16 16:14:35 -0500

    added beginning of mouse systems

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use std::path::{Path, PathBuf};
+use bevy::input::mouse::MouseButtonInput;
+use bevy::render::camera::Camera;
 
 use crate::gridcell::Pos;
 use bevy_svg::prelude::*;
@@ -10,11 +12,7 @@ fn make_scene(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     let root = PathBuf::from("tic-tac-toe/assets");
     let (red, blue, bbox) = {
-        let (mut red, mut blue, mut bbox) = (
-            root.clone(),
-            root.clone(),
-            root.clone(),
-        );
+        let (mut red, mut blue, mut bbox) = (root.clone(), root.clone(), root.clone());
 
         red.push("red_mark.svg");
         blue.push("blue_mark.svg");
@@ -34,8 +32,14 @@ fn make_scene(mut commands: Commands) {
         });
     });
 
-
     // commands.spawn_bundle(blue);
+}
+
+fn mouse(windows: Res<Windows>, camera: Query<&Transform, With<Camera>>) -> Vec2 {
+    Vec2::new(0.0, 0.0)
+}
+
+fn map_click_to_gridcell(pos: In<Vec2>, ev: EventReader<MouseButtonInput>) {
 }
 
 fn main() {
@@ -43,5 +47,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(SvgPlugin)
         .add_startup_system(make_scene.system())
+        .add_system(mouse.system().chain(map_click_to_gridcell.system()))
         .run()
 }
