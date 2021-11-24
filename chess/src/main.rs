06e9e49bf1980e7e87d8e0ba6298fbc06e6b5b23
zbrachinara <zbrachinara@gmail.com>
2021-11-23 20:54:16 -0500

    added boilerplate code to chess main.rs

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,3 +1,14 @@
+
+use bevy::prelude::*;
+
+fn hello_world(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    println!("Hello world!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(hello_world.system())
+        .run();
 }
