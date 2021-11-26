b698d922d839bea93abbf53b7049b0b0e630f5d0
zbrachinara <zbrachinara@gmail.com>
2021-11-26 13:57:20 -0500

    added example code

--- platformer/src/main.rs
+++ platformer/src/main.rs
@@ -1,3 +1,65 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+use bevy::render::pass::ClearColor;
+use nalgebra::Isometry2;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(ClearColor(Color::rgb(
+            0xF9 as f32 / 255.0,
+            0xF9 as f32 / 255.0,
+            0xFF as f32 / 255.0,
+        )))
+        .insert_resource(WindowDescriptor {
+            title: format!("win0"),
+            ..Default::default()
+        })
+        .insert_resource(Msaa::default())
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierRenderPlugin)
+        .add_startup_system(setup_graphics.system())
+        .add_startup_system(setup_physics.system())
+        .run();
+}
+
+fn setup_graphics(mut commands: Commands, mut configuration: ResMut<RapierConfiguration>) {
+    configuration.scale = 10.0;
+
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.transform = Transform::from_translation(Vec3::new(0.0, 200.0, 0.0));
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_translation(Vec3::new(1000.0, 10.0, 2000.0)),
+        light: Light {
+            intensity: 100_000_000_.0,
+            range: 6000.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+    commands.spawn_bundle(camera);
+}
+
+pub fn setup_physics(mut commands: Commands) {
+    commands
+        .spawn_bundle(ColliderBundle {
+            shape: ColliderShape::cuboid(1000.0, 1.0),
+            ..Default::default()
+        })
+        .insert(ColliderDebugRender::default())
+        .insert(ColliderPositionSync::Discrete);
+
+    commands
+        .spawn_bundle(RigidBodyBundle {
+            position: [0.0, 10.0].into(),
+            ..Default::default()
+        })
+        .insert_bundle(ColliderBundle {
+            shape: ColliderShape::cuboid(1.0, 1.0),
+            ..Default::default()
+        })
+        .insert(ColliderDebugRender::with_id(0))
+        .insert(ColliderPositionSync::Discrete);
+
 }
